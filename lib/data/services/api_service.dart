8727c53c7891c2e787a169590b2e151dd09e5453
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-10 12:25:08 +0700

    refactor(services): Modify apiService to be more general

--- lib/data/services/api_service.dart
+++ lib/data/services/api_service.dart
@@ -1,27 +1,28 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import '../models/movie_collection_model.dart';
 
 class ApiService {
   static const String _baseUrl = "https://api.themoviedb.org/3";
   static final String _apiKey = dotenv.env['TMDB_API_KEY'] ?? '';
 
-  static Future<MovieCollection> fetchData(String category) async {
-    final uri = Uri.parse('$_baseUrl/$category?api_key=$_apiKey');
+  /// Generic method to fetch data and deserialize it into the given model.
+  static Future<T> fetchData<T>(
+      String endpoint, T Function(Map<String, dynamic>) fromJson) async {
+    final uri = Uri.parse('$_baseUrl/$endpoint?api_key=$_apiKey');
 
     try {
       final response = await http.get(uri);
 
       if (response.statusCode == 200) {
         final jsonData = json.decode(response.body);
-        return MovieCollection.fromJson(jsonData);
+        return fromJson(jsonData);
       } else {
         throw Exception(
             "Error ${response.statusCode}: ${response.reasonPhrase}");
       }
     } catch (e) {
-      throw Exception("Failed to fetch movies: $e");
+      throw Exception("Failed to fetch data: $e");
     }
   }
 }
--- lib/data/services/movie_service.dart
+++ lib/data/services/movie_service.dart
@@ -4,7 +4,10 @@ import '../services/api_service.dart';
 class MovieService {
   static Future<MovieCollection> getMovies(String category) async {
     try {
-      return await ApiService.fetchData('movie/$category');
+      return await ApiService.fetchData(
+        'movie/$category',
+        (json) => MovieCollection.fromJson(json),
+      );
     } catch (e) {
       return MovieCollection(
           page: 1, movies: [], totalPages: 0, totalMovies: 0);
