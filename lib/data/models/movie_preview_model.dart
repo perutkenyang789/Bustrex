af0de2f2c3f5fbcfd21e3eb74193eed00d5dc16f
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-10 14:24:25 +0700

    fix(models): Adjust movie preview model to anticipate null movie backdrops

--- lib/data/models/movie_preview_model.dart
+++ lib/data/models/movie_preview_model.dart
@@ -1,13 +1,13 @@
 class MoviePreview {
   bool adult;
-  String backdropPath;
+  String? backdropPath;
   List<int> genreIds;
   int id;
   String originalLanguage;
   String originalTitle;
   String overview;
   double popularity;
-  String posterPath;
+  String? posterPath;
   DateTime releaseDate;
   String title;
   bool video;
@@ -16,14 +16,14 @@ class MoviePreview {
 
   MoviePreview({
     required this.adult,
-    required this.backdropPath,
+    this.backdropPath,
     required this.genreIds,
     required this.id,
     required this.originalLanguage,
     required this.originalTitle,
     required this.overview,
     required this.popularity,
-    required this.posterPath,
+    this.posterPath,
     required this.releaseDate,
     required this.title,
     required this.video,
@@ -33,14 +33,14 @@ class MoviePreview {
 
   factory MoviePreview.fromJson(Map<String, dynamic> json) => MoviePreview(
         adult: json["adult"],
-        backdropPath: json["backdrop_path"],
+        backdropPath: json["backdrop_path"] ?? '',
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
         id: json["id"],
         originalLanguage: json["original_language"],
         originalTitle: json["original_title"],
         overview: json["overview"],
         popularity: json["popularity"]?.toDouble(),
-        posterPath: json["poster_path"],
+        posterPath: json["poster_path"] ?? '',
         releaseDate: DateTime.parse(json["release_date"]),
         title: json["title"],
         video: json["video"],
