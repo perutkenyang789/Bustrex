036b417d48c13669c25aaf4f92aa6023bdef2ef3
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-10 19:47:49 +0700

    refactor(services): Remove debugging line from movie recommendation service

--- lib/data/services/movie_reccomendation_service.dart
+++ lib/data/services/movie_reccomendation_service.dart
@@ -4,17 +4,11 @@ import '../services/api_service.dart';
 class MovieRecommendationService {
   static Future<MovieCollection?> getMovieRecommendation(int id) async {
     try {
-      print("Fetching recommendations for movie ID: $id"); // Debugging line
-
-      final response = await ApiService.fetchData<MovieCollection>(
+      return await ApiService.fetchData<MovieCollection>(
         'movie/$id/recommendations',
         (json) => MovieCollection.fromJson(json),
       );
-
-      print("Response: $response"); // Debugging line
-      return response;
     } catch (e) {
-      print("Error fetching recommendations: $e");
       return null;
     }
   }
