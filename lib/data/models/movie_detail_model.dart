94bd77aeab29f9753807c609f4f317eaa5aeecaa
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-10 19:54:11 +0700

    refactor(models): Make sure one file only contain one class

--- lib/data/models/movie_detail_model.dart
+++ lib/data/models/movie_detail_model.dart
@@ -1,4 +1,9 @@
 import 'dart:convert';
+import 'belongs_to_collection_model.dart';
+import 'genre_model.dart';
+import 'production_company_model.dart';
+import 'production_country_model.dart';
+import 'spoken_language_model.dart';
 
 MovieDetail movieDetailFromJson(String str) =>
     MovieDetail.fromJson(json.decode(str));
@@ -8,7 +13,7 @@ String movieDetailToJson(MovieDetail data) => json.encode(data.toJson());
 class MovieDetail {
   bool adult;
   String backdropPath;
-  BelongsToCollection belongsToCollection;
+  BelongsToCollection? belongsToCollection;
   int budget;
   List<Genre> genres;
   String homepage;
@@ -36,7 +41,7 @@ class MovieDetail {
   MovieDetail({
     required this.adult,
     required this.backdropPath,
-    required this.belongsToCollection,
+    this.belongsToCollection,
     required this.budget,
     required this.genres,
     required this.homepage,
@@ -65,8 +70,9 @@ class MovieDetail {
   factory MovieDetail.fromJson(Map<String, dynamic> json) => MovieDetail(
         adult: json["adult"],
         backdropPath: json["backdrop_path"],
-        belongsToCollection:
-            BelongsToCollection.fromJson(json["belongs_to_collection"]),
+        belongsToCollection: json["belongs_to_collection"] == null
+            ? null
+            : BelongsToCollection.fromJson(json["belongs_to_collection"]),
         budget: json["budget"],
         genres: List<Genre>.from(json["genres"].map((x) => Genre.fromJson(x))),
         homepage: json["homepage"],
@@ -100,7 +106,7 @@ class MovieDetail {
   Map<String, dynamic> toJson() => {
         "adult": adult,
         "backdrop_path": backdropPath,
-        "belongs_to_collection": belongsToCollection.toJson(),
+        "belongs_to_collection": belongsToCollection?.toJson(),
         "budget": budget,
         "genres": List<dynamic>.from(genres.map((x) => x.toJson())),
         "homepage": homepage,
@@ -128,128 +134,5 @@ class MovieDetail {
         "video": video,
         "vote_average": voteAverage,
         "vote_count": voteCount,
-      };
-}
-
-class BelongsToCollection {
-  int id;
-  String name;
-  String posterPath;
-  String backdropPath;
-
-  BelongsToCollection({
-    required this.id,
-    required this.name,
-    required this.posterPath,
-    required this.backdropPath,
-  });
-
-  factory BelongsToCollection.fromJson(Map<String, dynamic> json) =>
-      BelongsToCollection(
-        id: json["id"],
-        name: json["name"],
-        posterPath: json["poster_path"],
-        backdropPath: json["backdrop_path"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "poster_path": posterPath,
-        "backdrop_path": backdropPath,
-      };
-}
-
-class Genre {
-  int id;
-  String name;
-
-  Genre({
-    required this.id,
-    required this.name,
-  });
-
-  factory Genre.fromJson(Map<String, dynamic> json) => Genre(
-        id: json["id"],
-        name: json["name"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-      };
-}
-
-class ProductionCompany {
-  int id;
-  String? logoPath;
-  String name;
-  String originCountry;
-
-  ProductionCompany({
-    required this.id,
-    required this.logoPath,
-    required this.name,
-    required this.originCountry,
-  });
-
-  factory ProductionCompany.fromJson(Map<String, dynamic> json) =>
-      ProductionCompany(
-        id: json["id"],
-        logoPath: json["logo_path"],
-        name: json["name"],
-        originCountry: json["origin_country"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "logo_path": logoPath,
-        "name": name,
-        "origin_country": originCountry,
-      };
-}
-
-class ProductionCountry {
-  String iso31661;
-  String name;
-
-  ProductionCountry({
-    required this.iso31661,
-    required this.name,
-  });
-
-  factory ProductionCountry.fromJson(Map<String, dynamic> json) =>
-      ProductionCountry(
-        iso31661: json["iso_3166_1"],
-        name: json["name"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "iso_3166_1": iso31661,
-        "name": name,
-      };
-}
-
-class SpokenLanguage {
-  String englishName;
-  String iso6391;
-  String name;
-
-  SpokenLanguage({
-    required this.englishName,
-    required this.iso6391,
-    required this.name,
-  });
-
-  factory SpokenLanguage.fromJson(Map<String, dynamic> json) => SpokenLanguage(
-        englishName: json["english_name"],
-        iso6391: json["iso_639_1"],
-        name: json["name"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "english_name": englishName,
-        "iso_639_1": iso6391,
-        "name": name,
       };
 }
