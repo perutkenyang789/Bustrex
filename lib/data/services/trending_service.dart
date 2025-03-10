abf5bb8fd47789a8009851d54156579b71111ffa
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2025-03-10 14:21:23 +0700

    refactor(services): Make adjustments to trending service due to api service modification

--- lib/data/services/trending_service.dart
+++ lib/data/services/trending_service.dart
@@ -4,7 +4,8 @@ import '../services/api_service.dart';
 class TrendingService {
   static Future<MovieCollection> getHighlights(String category) async {
     try {
-      return await ApiService.fetchData('trending/$category/day');
+      return await ApiService.fetchData(
+          'trending/$category/day', (json) => MovieCollection.fromJson(json));
     } catch (e) {
       return MovieCollection(
           page: 1, movies: [], totalPages: 0, totalMovies: 0);
